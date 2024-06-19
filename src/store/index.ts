618d6c77cc784631d3be83d80c6bec73730748f6
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-19 12:21:25 -0500

    feat: usersSlice config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import usersSlice  from "./slices/usersSlice";
 export const store = configureStore({
     reducer: {
-        usersReducer: {},
+        usersReducer: usersSlice,
     },    
 });
