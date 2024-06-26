dc4a8dcf58ce8af6e4a7104a8744929478f300f9
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-26 12:59:31 -0500

    feat: header implementation in layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from "react";
+import Header from "../Header/Header";
+import { useSelector } from "react-redux";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const auth = useSelector((state: any) => state.auth);
+
   return (
-    <div className="bg-gradient-to-r from-wood via-gold to-wood-darker min-h-screen flex items-center justify-center p-4">
-      <div className="w-full max-w-md">{children}</div>
+    <div className="bg-gradient-to-r from-wood via-gold to-wood-darker min-h-screen flex flex-col">
+      <Header isLogged={auth.isLogged} userName={auth.userName} />
+      <div className="flex-grow p-4">{children}</div>
     </div>
   );
 };
