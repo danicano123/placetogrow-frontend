b048f9832d77826de7e3767ca8070fa2cb8b7fe5
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-28 00:25:04 -0500

    feat: patch method implemented

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,4 +1,3 @@
-
 export class Api {
   static baseUrl = "http://localhost:8000/api/v1";
 
@@ -17,29 +16,65 @@ export class Api {
     };
   }
 
-  static async get(url: string, token?: string): Promise<{ statusCode: number; data: any }> {
+  static async get(
+    url: string,
+    token?: string
+  ): Promise<{ statusCode: number; data: any }> {
     const headers: Record<string, string> = {
       "Content-Type": "application/json",
     };
-  
+
     if (token) {
       headers["Authorization"] = `Bearer ${token}`;
     }
-  
+
     try {
       const response = await fetch(`${Api.baseUrl}${url}`, {
         method: "GET",
         headers: headers,
       });
-  
+
       const fetchResult = await response.json();
-  
+
       return {
         statusCode: response.status,
         data: fetchResult,
       };
     } catch (error) {
       console.error("Error fetching data:", error);
+      throw error;
+    }
+  }
+
+  // Añadir el método PATCH
+  static async patch(
+    url: string,
+    data: any,
+    token?: string
+  ): Promise<{ statusCode: number; data: any }> {
+    const headers: Record<string, string> = {
+      "Content-Type": "application/json",
+    };
+
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    try {
+      const response = await fetch(`${Api.baseUrl}${url}`, {
+        method: "PATCH",
+        headers: headers,
+        body: JSON.stringify(data),
+      });
+
+      const fetchResult = await response.json();
+
+      return {
+        statusCode: response.status,
+        data: fetchResult,
+      };
+    } catch (error) {
+      console.error("Error updating data:", error);
       throw error;
     }
   }
