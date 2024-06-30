bec09faa48ff72960bca6c78a668b0a9df601420
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-30 13:39:33 -0500

    feat: edit users view available on router

--- src/routes/RoutesSchema.tsx
+++ src/routes/RoutesSchema.tsx
@@ -2,12 +2,17 @@ import Dashboard from "../components/Dashboard/Dashboard";
 import RegisterForm from "../components/Register/RegisterForm";
 import Login from "../views/Login";
 import MicrositesIndex from "../views/Microsites/MicrositesIndex";
+import EditUser from "../views/Users/EditUsers";
 import UsersDashboard from "../views/Users/UsersDashboard";
 import Welcome from "../views/Welcome";
 
 export const RoutesSchema = [
   {
     path: "/",
+    element: Welcome,
+  },
+  {
+    path: "*",
     element: Welcome,
   },
   {
@@ -26,7 +31,10 @@ export const RoutesSchema = [
       {
         path: "users",
         element: UsersDashboard,
-        isProtected: true,
+      },
+      {
+        path: "users/:id",
+        element: EditUser,
       },
       
     ]
