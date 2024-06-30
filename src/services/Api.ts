138d68790132133509b418c8d1c3b5d3e8dd5fb7
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-30 13:38:13 -0500

    feat: post method now implements token

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,19 +1,32 @@
 export class Api {
   static baseUrl = "http://localhost:8000/api/v1";
 
-  static async post<T>(url: string, data: T): Promise<any> {
-    const response = await fetch(`${Api.baseUrl}${url}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-
-    const fetchResult = await response.json();
-
-    return {
-      statusCode: response.status,
-      data: fetchResult,
+  static async post<T>(url: string, data: T, token?: string): Promise<any> {
+    const headers: Record<string, string> = {
+      "Content-Type": "application/json",
     };
+
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    try {
+      const response = await fetch(`${Api.baseUrl}${url}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      });
+
+      const fetchResult = await response.json();
+
+      return {
+        statusCode: response.status,
+        data: fetchResult,
+      };
+    } catch (error) {
+      console.error("Error posting data:", error);
+      throw error;
+    }
   }
 
   static async get(
