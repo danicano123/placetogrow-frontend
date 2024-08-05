77b10118fde7ba3e558a2efe3fc29b790386a666
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-08-05 03:33:44 -0500

    feat: forms and payments routed added

--- src/routes/RoutesSchema.tsx
+++ src/routes/RoutesSchema.tsx
@@ -1,11 +1,15 @@
 import Dashboard from "../components/Dashboard/Dashboard";
 import RegisterForm from "../components/Register/RegisterForm";
+import EditForm from "../views/Forms/EditForm";
+import PaymentForm from "../views/Forms/PaymentForm";
 import Login from "../views/Login";
 import CreateMicrosite from "../views/Microsites/CreateMicrosite";
 import EditMicrosite from "../views/Microsites/EditMicrosites";
 import MicrositesDashboard from "../views/Microsites/MicrositesDashboard";
 import MicrositeDetail from "../views/Microsites/MicrositesDetail";
 import MicrositesList from "../views/Microsites/MicrositesList";
+import PaymentsDashboard from "../views/Payments/PaymentsDashboard";
+import PaymentsList from "../views/Payments/PaymentsList";
 import EditUser from "../views/Users/EditUsers";
 import UsersDashboard from "../views/Users/UsersDashboard";
 
@@ -20,6 +24,11 @@ export const RoutesSchema = [
     isProtected: true,
   },
   {
+    path: "/microsites/:slug/form/:micrositeId",
+    element: PaymentForm,
+    isProtected: true,
+  },
+  {
     path: "*",
     element: MicrositesList,
   },
@@ -30,6 +39,11 @@ export const RoutesSchema = [
   {
     path: "/register",
     element: RegisterForm,
+  },
+  {
+    path: "/payments",
+    element: PaymentsList,
+    isProtected: true,
   },
   {
     path: "/dashboard",
@@ -56,6 +70,14 @@ export const RoutesSchema = [
       {
         path: "microsites/create-microsite",
         element: CreateMicrosite,
+      },
+      {
+        path: "microsites/form/:microsite_id",
+        element: EditForm,
+      },
+      {
+        path: "microsites/payments/:micrositeId",
+        element: PaymentsDashboard,
       },
     ],
   },
