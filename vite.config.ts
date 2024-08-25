b8712e04f3b51b0c62a7e80b8c02a0550c3aaa2f
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-08-25 03:04:20 -0500

    feat: now you can choose the port of the app using .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(process.env.VITE_PORT) || 5173,
+    },
+  };
+});
